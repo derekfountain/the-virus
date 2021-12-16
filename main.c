967826d841fc0fd3b9fc3f25dc0eb4cdca24a90d
Derek Fountain <derek@scratters.com>
2021-12-16 15:51:15 +0000

    work every other dot each iteration, makes it faster. small other tweaks

--- main.c
+++ main.c
@@ -72,7 +72,7 @@ void main(void)
   /* Create a pool of random numbers for the jitter. These are added to the velocity */
   for(i=0;i<256;i++)
   {
-    random_values[i] = rand()%100;
+    random_values[i] = rand()%50;
     if( i & 0x01 )
       random_values[i] = -(random_values[i]);
     // printf("%d ", random_values[i]);
@@ -110,19 +110,31 @@ void main(void)
     if( in_key_pressed( IN_KEY_SCANCODE_o ) && player_x_i )       player_x_i-=2;
     if( in_key_pressed( IN_KEY_SCANCODE_p ) && player_x_i < 254 ) player_x_i+=2;
 
-    for( i=0; i < MAX_IN_SWARM; i++ )
+    /*
+     * Update each dot's velocity and then location based on empirically derived rules. This actually
+     * updates every other dot each pass, every other-other dot being left to do the same as it did
+     * in the previous iteration. This allows twice as many dots to be managed, at the expense of
+     * more accurate behaviour.
+     */
+    static uint8_t every_other_dot=0;
+    every_other_dot ^= 1;
+    for( i=every_other_dot; i < MAX_IN_SWARM; i+=2 )
     {
       /*
        * Move towards player. Calculate distance to player, take 1/256th of it. Add that to velocity.
+       * This has to be a signed 32bit calculation. If the player is a x=255 then 255*256=65280. If
+       * the player is at x=0 then 0*0=0, and if the swarm dot is at x=255 then 0-65280=-65280. The
+       * result will be in the range 255 to -255.
        */
-      swarm[i].velocity_x += (player_x_i*(int16_t)256 - swarm[i].x_i*(int16_t)256)/(int16_t)256;
-      swarm[i].velocity_y += (player_y_i*(int16_t)256 - swarm[i].y_i*(int16_t)256)/(int16_t)256;
+      swarm[i].velocity_x += (((int32_t)player_x_i*(int32_t)256) - (int32_t)swarm[i].x_i*(int32_t)256) / (int16_t)256;
+      swarm[i].velocity_y += (((int32_t)player_y_i*(int32_t)256) - (int32_t)swarm[i].y_i*(int32_t)256) / (int16_t)256;
 
 
       /*
-       * Limit velocity
+       * Limit velocity. This ugly and causes stuttering sort of behaviour, but it's the best
+       * I could get without a square root.
        */
-      const int16_t SPEED_LIMIT = 400;
+      const int16_t SPEED_LIMIT = 350;
       if( swarm[i].velocity_x > SPEED_LIMIT || swarm[i].velocity_x < -SPEED_LIMIT )
       {
 	if( swarm[i].velocity_x / 2 != 0 )
@@ -136,15 +148,20 @@ void main(void)
 
 
       /*
-       * Original algorithm attempts to move dots so they don't get too close
-       * to each other. That's not posssible on the Spectrum. I get the milling
-       * around behaviour by introducing some random jitter.
+       * Original algorithm attempts to move dots so they don't get too close to each other.
+       * That's not posssible on the Spectrum, there's nowhere near enough CPU power to
+       * iterate all of the swarm each iteration of this loop. I get the milling around behaviour
+       * by introducing some random jitter.
        */
       swarm[i].velocity_x += *(random_values+(rand()/256));
       swarm[i].velocity_y += *(random_values+(rand()/256));
 
 
-      /* Finally, add calculated velocity to dot position */
+      /*
+       * Finally, add calculated velocity to dot position. The velocity calculation is done at high
+       * scale to maintain accuracy in the integers. (Doing maths with numbers in the range 0-255
+       * leads to rounding to zeroes way too much.) So this takes 1% of the velocity as calculated.
+       */
       swarm[i].x_i += swarm[i].velocity_x/100;
       swarm[i].y_i += swarm[i].velocity_y/100;
     }
